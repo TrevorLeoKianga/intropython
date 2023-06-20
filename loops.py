ddfaf1a40b1b238e00cea4e0f4612e51d2e4e065
emobilis <kiangatrevor99@gmail.com>
2023-06-20 17:21:40 +0300

    python

---
+++ loops.py
@@ -0,0 +1,17 @@
+# 1. WHILE LOOP
+counter = 0
+while counter <= 50:
+    print(counter)
+    counter += 8
+
+t = 0
+while range(t, 10):
+    print(t)
+    t += 1
+
+
+counter2 = 20
+while counter2 >= 15:
+    print(counter2)
+    counter2 += -2
+
